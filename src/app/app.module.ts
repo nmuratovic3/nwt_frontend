e38f9860076f52c4fe2001373b1e520dfe7e81da
Nedina Muratovic <nmuratovic3@etf.unsa.ba>
2023-05-19 17:04:30 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartComponent } from './components/cart/cart.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    HeaderComponent,
+    ProductListComponent,
+    CartComponent,
+    OrderHistoryComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
