b9cbbb94799dd21a75d2ee7491b8fbb8af4e086f
Nedina Muratovic <nmuratovic3@etf.unsa.ba>
2023-06-04 23:54:02 +0200

    get products from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from'@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
@@ -16,6 +16,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
+    HttpClientModule,
     AppComponent,
     ProductComponent,
     HeaderComponent,
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
-
+export class ProductListComponent implements OnInit {
+  shoeList: any;
+  constructor(private httpClient: HttpClient){
+    this.shoeList=[];
+  }
+ ngOnInit(): void{
+  this.getProductList()
+ }
+ getProductList(){
+  this.httpClient.get('').subscribe((result: any)=>{
+  this.shoeList=result //Staviti API
+})
+ }
 }
