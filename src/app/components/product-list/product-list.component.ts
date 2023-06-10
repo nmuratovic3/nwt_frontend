9b1af28091d58db2c2edc4fddc9edad90881454c
Nedina Muratovic <nmuratovic3@etf.unsa.ba>
2023-06-10 15:31:02 +0200

    radi

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -9,7 +9,8 @@ import { ProductService } from 'src/app/services/ProductService';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {}
+/*export class ProductListComponent implements OnInit {
   shoeList!: Product[];
 
   constructor(private productService: ProductService){
@@ -21,4 +22,4 @@ export class ProductListComponent implements OnInit {
   })
  }
 
-}
+}*/
